58cc72d4917c52e3909e601ebdb93003658398c1
imodclub <70686000+imodclub@users.noreply.github.com>
2022-08-22 16:03:32 +0700

    edit from adduserform

--- estock/src/Components/AddUserForm.js
+++ estock/src/Components/AddUserForm.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useState } from 'react'
-import { Container, TextField, Box } from '@mui/material'
+import { TextField, Box } from '@mui/material'
 import { styled } from '@mui/material/styles';
+import Autocomplete from '@mui/material/Autocomplete';
 import Signin from './Signin';
 
 const Div = styled('div')(({ theme }) => ({
@@ -9,66 +10,75 @@ const Div = styled('div')(({ theme }) => ({
   padding: theme.spacing(1),
 }));
 
+const listFname = [{ label: "นาย" }, { label: "นางสาว" }, { label: "นาง" }, { label: "ศ." }, { label: "ศ.ดร." }, { label: "รศ." }, { label: "รศ.ดร." },
+  { label: "ผศ." }, { label: "ผศ.ดร." }, { label: "ดร." }];
+
 function AddUserForm(props) {
   const db = props.db;
   const [loading, setLoading] =  useState(false)
     
     return (
       <Fragment>
-          <Box
-            sx={{ mt: 1, display: 'flex', maxWidth: '100%' }}
-          >
-            <TextField
-              variant="outlined"
-              sx={{ maxWidth: '20%', padding: 1 }}
-              label={<Div>คำนำหน้า</Div>}
-              id="fname"
-            />
-            <TextField
-              sx={{ maxWidth: '40%', padding: 1 }}
-              label={<Div>ชื่อ</Div>}
-              id="name"
-            />
-            <TextField
-              sx={{ maxWidth: '40%', padding: 1 }}
-              label={<Div>นามสกุล</Div>}
-              id="lname"
-            />
-          </Box>
-          <Box
-            sx={{ mt: 1, display: 'flex', maxWidth: '100%' }}
-          >
-            <TextField
-              sx={{ maxWidth: '55%', padding: 1 }}
-              label={<Div>หน่วยงาน/ภาควิชา</Div>}
-              id="departments"
-            />
-            <TextField
-              sx={{ maxWidth: '55%', padding: 1 }}
-              label={<Div>โทรศัพท์ภายใน</Div>}
-              id="telinternel"
-            />
-          </Box>
-          <Box
-            sx={{ mt: 1, display: 'flex', maxWidth: '100%' }}
-          >
-            <TextField
+        <Box sx={{ mt: 1, display: 'flex' }}>
+          <Autocomplete
+            disablePortal
+            id="fname"
+            options={listFname}
+            sx={{ width:200,padding: 1 }}
+            
+            renderInput={(params) => <TextField {...params} label="คำนำหน้า" />}
+          ></Autocomplete>
+        </Box>
+        <Box sx={{ mt: 1, display: 'flex' }}>
+          <TextField
+            sx={{ maxWidth: '70%', padding: 1 }}
+            label={<Div>ชื่อ</Div>}
+            id="name"
+          />
+          <TextField
+            sx={{ maxWidth: '70%', padding: 1 }}
+            label={<Div>นามสกุล</Div>}
+            id="lname"
+          />
+        </Box>
+        <Box sx={{ mt: 1, display: 'flex' }}>
+          <TextField
+            sx={{ maxWidth: '55%', padding: 1 }}
+            label={<Div>หน่วยงาน/ภาควิชา</Div>}
+            id="departments"
+          />
+          <TextField
+            sx={{ maxWidth: '55%', padding: 1 }}
+            label={<Div>โทรศัพท์ภายใน</Div>}
+            id="telinternel"
+          />
+        </Box>
+        <Box sx={{ mt: 1, display: 'flex' }}>
+          <TextField
             sx={{ maxWidth: '100%', padding: 1 }}
             fullWidth
-              label={<Div>โทรศัพท์ที่ติดต่อได้ (ไม่บังคับ)</Div>}
-              id="privatetel"
-            />
-          </Box>
-          <Box
-            sx={{ mt: 1, display: 'flex', maxWidth: '100%' }}
-          >
-            <TextField
+            label={<Div>Email</Div>}
+            id="email"
+          />
+        </Box>
+        <Box sx={{ mt: 1, display: 'flex' }}>
+          <TextField
+            sx={{ maxWidth: '100%', padding: 1 }}
+            fullWidth
+            label={<Div>โทรศัพท์ที่ติดต่อได้ (ไม่บังคับ)</Div>}
+            id="privatetel"
+          />
+        </Box>
+        <Box sx={{ mt: 1, display: 'flex' }}>
+          <TextField
             sx={{ maxWidth: 'auto', padding: 1 }}
             fullWidth
-              label={<Div>ช่องทางติดต่ออื่นๆ เช่น โซเซียลมีเดีย Line (ไม่บังคับ)</Div>}
-              id="social"
-            />
-          </Box>
+            label={
+              <Div>ช่องทางติดต่ออื่นๆ เช่น โซเซียลมีเดีย Line (ไม่บังคับ)</Div>
+            }
+            id="social"
+          />
+        </Box>
       </Fragment>
     );
 }
