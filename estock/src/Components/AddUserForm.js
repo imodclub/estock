513c033098a4fca5bbbe0a82580744dc0bd62f6f
imodclub <70686000+imodclub@users.noreply.github.com>
2022-08-24 14:30:40 +0700

    user form success

--- estock/src/Components/AddUserForm.js
+++ estock/src/Components/AddUserForm.js
@@ -3,7 +3,7 @@ import { TextField, Box,Button,Grid } from '@mui/material'
 import { styled } from '@mui/material/styles';
 import Autocomplete from '@mui/material/Autocomplete';
 import Signin from './Signin';
-import { Container } from 'react-bootstrap';
+import { collection, addDoc, Timestamp, serverTimestamp } from 'firebase/firestore'
 
 const Div = styled('div')(({ theme }) => ({
   ...theme.typography.button,
@@ -42,15 +42,13 @@ function AddUserForm(props) {
   const textInputDepartments = useRef(null);
 
   //ref
-  const {
-    inputTextName,
-    inputTextLastname,
-    inputTextDepartments,
-    inputTextTelinternal,
-    inputTextTelPrivate,
-    inputTextEmail,
-    inputTextSocial,
-  } = useRef(null);
+  const inputTextName = useRef(null);
+  const inputTextLastname = useRef(null);
+  const inputTextEmail = useRef(null);
+  const inputTextDepartments = useRef(null);
+  const inputTextTelInternal = useRef(null);
+  const inputTextTelPrivate = useRef(null);
+  const inputTextSocial = useRef(null);
 
   useEffect(() => {
     const validator =
@@ -75,124 +73,147 @@ function AddUserForm(props) {
   const handleChangeTelPrivate= (e) => {setTelPrivate(e)};
   const handleChangeSocial = (e) => { setSocial(e) };
   
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log("คำนำหน้า ",fname)
-    console.log("ชื่อ ",name)
-    console.log("นามสกุล ",lastname)
-    console.log("อีเมล ",email)
-    console.log("แผนก ",departments)
-    console.log('โทร ', telinternel);
+    const docRef = await addDoc(collection(db, "User"), {
+      Fname: fname,
+      Name: name,
+      Lastname: lastname,
+      Email: email,
+      Departments: departments,
+      TelOfUBU: telinternel,
+      TelPrivate: telPrivate,
+      Social: social,
+      CreateUserDate:serverTimestamp()
+    }).then((doc) => {
+      alert('บันทึกข้อมูลสำเร็จ')
+      window.location.reload();
+    }).catch((error)=>{
+      console.log("ไม่สามารถเชื่อมต่อฐานข้อมูลได้");
+      console.log(error)
+    })
+    
   }
+
   const handleClear = (e) => {
     e.preventDefault();
-    inputTextName.current.value=''
+    inputTextName.current.value="";
+    inputTextLastname.current.value="";
+    inputTextEmail.current.value = "";
+    inputTextDepartments.current.value = '';
+    inputTextTelInternal.current.value="";
+    inputTextTelPrivate.current.value="";
+    inputTextSocial.current.value="";
   }
 
     return (
       <Fragment>
         <Box sx={{ mt: 1, display: 'flex' }}>
-            <Autocomplete
-              value={value}
-              onChange={(event, newValue) => {
-                setValue(newValue);
-                setFname(newValue);
-              }}
-              inputValue={inputValue}
-              onInputChange={(event, newInputValue) => {
-                setInputValue(newInputValue);
-              }}
-              selectOnFocus
-              id="fnamelist"
-              options={options}
-              sx={{ width: 200, padding: 1 }}
-              renderInput={(params) => (
-                <TextField {...params} label="คำนำหน้า" id="fname" />
-              )}
-            />
-          </Box>
-          <Box sx={{ mt: 1, display: 'flex' }}>
-            <TextField
-              sx={{ maxWidth: '70%', padding: 1 }}
-              label={<Div>ชื่อ</Div>}
+          <Autocomplete
+            value={value}
+            onChange={(event, newValue) => {
+              setValue(newValue);
+              setFname(newValue);
+            }}
+            inputValue={inputValue}
+            onInputChange={(event, newInputValue) => {
+              setInputValue(newInputValue);
+            }}
+            selectOnFocus
+            id="fnamelist"
+            options={options}
+            sx={{ width: 200, padding: 1 }}
+            renderInput={(params) => (
+              <TextField {...params} label="คำนำหน้า" id="fname" />
+            )}
+          />
+        </Box>
+        <Box sx={{ mt: 1, display: 'flex' }}>
+          <TextField
+            sx={{ maxWidth: '70%', padding: 1 }}
+            label={<Div>ชื่อ</Div>}
             id="name"
             inputRef={inputTextName}
             onChange={(e) => handleChangeName(e.target.value)}
-            />
-            <TextField
-              sx={{ maxWidth: '70%', padding: 1 }}
-              label={<Div>นามสกุล</Div>}
-              id="lname"
-              onChange={(e) => handleChangeLastname(e.target.value)}
-            />
+          />
+          <TextField
+            sx={{ maxWidth: '70%', padding: 1 }}
+            label={<Div>นามสกุล</Div>}
+            id="lname"
+            inputRef={inputTextLastname}
+            onChange={(e) => handleChangeLastname(e.target.value)}
+          />
+        </Box>
+        <Box sx={{ mt: 1, display: 'flex' }}>
+          <TextField
+            sx={{ maxWidth: '55%', padding: 1 }}
+            label={<Div>หน่วยงาน/ภาควิชา</Div>}
+            id="departments"
+            inputRef={inputTextDepartments}
+            onChange={(e) => handleChangeDepartments(e.target.value)}
+          />
+          <TextField
+            sx={{ maxWidth: '55%', padding: 1 }}
+            label={<Div>โทรศัพท์ภายใน</Div>}
+            id="telinternel"
+            inputRef={inputTextTelInternal}
+            onChange={(e) => handleChangeTelIternal(e.target.value)}
+          />
+        </Box>
+        <Box sx={{ mt: 1, display: 'flex' }}>
+          <TextField
+            sx={{ maxWidth: '100%', padding: 1 }}
+            fullWidth
+            label={<Div>Email</Div>}
+            id="email"
+            inputRef={inputTextEmail}
+            onChange={(e) => handleChangeEmail(e.target.value)}
+          />
+        </Box>
+        <Box sx={{ mt: 1, display: 'flex' }}>
+          <TextField
+            sx={{ maxWidth: '100%', padding: 1 }}
+            fullWidth
+            label={<Div>โทรศัพท์ที่ติดต่อได้ (ไม่บังคับ)</Div>}
+            id="telprivate"
+            inputRef={inputTextTelPrivate}
+            onChange={(e) => handleChangeTelPrivate(e.target.value)}
+          />
+        </Box>
+        <Box sx={{ mt: 1, display: 'flex' }}>
+          <TextField
+            sx={{ maxWidth: 'auto', padding: 1 }}
+            fullWidth
+            label={
+              <Div>ช่องทางติดต่ออื่นๆ เช่น โซเซียลมีเดีย Line (ไม่บังคับ)</Div>
+            }
+            id="social"
+            inputRef={inputTextSocial}
+            onChange={(e) => handleChangeSocial(e.target.value)}
+          />
+        </Box>
+        <Box sx={{ mt: 1 }}>
+          <Box component="div" sx={{ display: 'inline', p: 1, m: 1 }}>
+            <Button
+              sx={{ padding: 1 }}
+              onClick={handleClear}
+              variant="outlined"
+              color="error"
+            >
+              Clear
+            </Button>
           </Box>
-          <Box sx={{ mt: 1, display: 'flex' }}>
-            <TextField
-              sx={{ maxWidth: '55%', padding: 1 }}
-              label={<Div>หน่วยงาน/ภาควิชา</Div>}
-              id="departments"
-              onChange={(e) => handleChangeDepartments(e.target.value)}
-            />
-            <TextField
-              sx={{ maxWidth: '55%', padding: 1 }}
-              label={<Div>โทรศัพท์ภายใน</Div>}
-              id="telinternel"
-              onChange={(e) => handleChangeTelIternal(e.target.value)}
-            />
-          </Box>
-          <Box sx={{ mt: 1, display: 'flex' }}>
-            <TextField
-              sx={{ maxWidth: '100%', padding: 1 }}
-              fullWidth
-              label={<Div>Email</Div>}
-              id="email"
-              onChange={(e) => handleChangeEmail(e.target.value)}
-            />
-          </Box>
-          <Box sx={{ mt: 1, display: 'flex' }}>
-            <TextField
-              sx={{ maxWidth: '100%', padding: 1 }}
-              fullWidth
-              label={<Div>โทรศัพท์ที่ติดต่อได้ (ไม่บังคับ)</Div>}
-              id="telprivate"
-              onChange={(e) => handleChangeTelPrivate(e.target.value)}
-            />
-          </Box>
-          <Box sx={{ mt: 1, display: 'flex' }}>
-            <TextField
-              sx={{ maxWidth: 'auto', padding: 1 }}
-              fullWidth
-              label={
-                <Div>
-                  ช่องทางติดต่ออื่นๆ เช่น โซเซียลมีเดีย Line (ไม่บังคับ)
-                </Div>
-              }
-              id="social"
-              onChange={(e) => handleChangeSocial(e.target.value)}
-            />
-          </Box>
-          <Box sx={{ mt: 1 }}>
-            <Box component="div" sx={{ display: 'inline', p: 1, m: 1 }}>
-              <Button
-                sx={{ padding: 1 }}
-                onClick={handleClear}
-                variant="outlined"
-                color="error"
-              >
-                Clear
-              </Button>
-            </Box>
-            <Box component="div" sx={{ display: 'inline', p: 1, m: 1 }}>
-              <Button
-                variant="outlined"
+          <Box component="div" sx={{ display: 'inline', p: 1, m: 1 }}>
+            <Button
+              variant="outlined"
               sx={{ padding: 1 }}
               disabled={!error}
-                onClick={handleSubmit}
-              >
-                เพิ่มข้อมูล
-              </Button>
+              onClick={handleSubmit}
+            >
+              เพิ่มข้อมูล
+            </Button>
           </Box>
-          </Box>
+        </Box>
       </Fragment>
     );
 }
