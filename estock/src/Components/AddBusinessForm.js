c5bf1d42a57da9e4c306684c9641e2ecb1d25f6b
imodclub <70686000+imodclub@users.noreply.github.com>
2022-08-31 14:57:09 +0700

    add business form susscess

--- estock/src/Components/AddBusinessForm.js
+++ estock/src/Components/AddBusinessForm.js
@@ -19,6 +19,8 @@ function AddUserForm(props) {
   const [inputValue, setInputValue] = useState('');
   const [businessName, setBusinessName] = useState(null);
   const [businessAddress, setBusinessAddress] = useState(null);
+  const [soi, setSoi] = useState(null);
+  const [road, setRoad] = useState(null);
   const [subdistrict, setSubdistrict] = useState(null);
   const [district, setDistrict] = useState(null);
   const [province, setProvince] = useState(null);
@@ -27,55 +29,80 @@ function AddUserForm(props) {
 
 
   //ref
-  const inputTextName = useRef(null);
-  const inputTextLastname = useRef(null);
-  const inputTextEmail = useRef(null);
-  const inputTextDepartments = useRef(null);
-  const inputTextTelInternal = useRef(null);
-  const inputTextTelPrivate = useRef(null);
-  const inputTextSocial = useRef(null);
+  const inputTextBusinessName = useRef(null);
+  const inputTextBusinessAddress = useRef(null);
+  const inputTextSoi = useRef(null);
+  const inputTextRoad = useRef(null);
+  const inputTextSubdistrict = useRef(null);
+  const inputTextDistrict = useRef(null);
+  const inputTextProvince = useRef(null);
+  const inputTextZipcode = useRef(null);
+  const inputTextTelephone = useRef(null);
 
   
   const handleChangeBusinessName=(e)=>{setBusinessName(e)}
   const handleChangeBusinessAddress=(e)=>{setBusinessAddress(e)}
+  const handleChangeSoi = (e) => { setSoi(e) };
+  const handleChangeRoad = (e) => {setRoad(e);};
   const handleChangeSubdistrict = (e) => { setSubdistrict(e) }
   const handleChangeDistrict = (e) => { setDistrict(e) }
   const handleChangeProvince = (e) => { setProvince(e)}
   const handleChangeZipcode= (e) => {setZipcode(e)};
-  const  handleChangeTelephone = (e) => { setTelephone(e) };
+  const handleChangeTelephone = (e) => { setTelephone(e) };
+
+  useEffect(() => {
+    const validator =
+      businessName?.trim().length > 0 &&
+      businessAddress?.trim().length > 0 &&
+      soi?.trim().length > 0 &&
+      road?.trim().length > 0 &&
+      subdistrict?.trim().length > 0 &&
+      district?.trim().length > 0 &&
+      province?.trim().length > 0 &&
+      zipcode > 0 &&
+      telephone;
+    if (validator) {
+      setError(true);
+    } else {
+      setError(false);
+    }
+  }, [businessName, businessAddress,soi,road, subdistrict, district, province, zipcode,telephone]);
   
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    /*const docRef = await addDoc(collection(db, "User"), {
-      Fname: fname,
-      Name: name,
-      Lastname: lastname,
-      Email: email,
-      Departments: departments,
-      TelOfUBU: telinternel,
-      TelPrivate: telPrivate,
-      Social: social,
-      CreateUserDate:serverTimestamp()
-    }).then((doc) => {
-      alert('บันทึกข้อมูลสำเร็จ')
-      window.location.reload();
-    }).catch((error)=>{
-      console.log("ไม่สามารถเชื่อมต่อฐานข้อมูลได้");
-      console.log(error)
+    const docRef = await addDoc(collection(db, 'Business'), {
+      BusinessName: businessName,
+      BusinessAddress: businessAddress,
+      Road:road,
+      Subdistrict: subdistrict,
+      District: district,
+      Province: province,
+      Zipcode: zipcode,
+      Telephone: telephone,
+      CreateUserDate: serverTimestamp(),
     })
-    */
+      .then((doc) => {
+        alert('บันทึกข้อมูลสำเร็จ');
+        window.location.reload();
+      })
+      .catch((error) => {
+        console.log('ไม่สามารถเชื่อมต่อฐานข้อมูลได้');
+        console.log(error);
+      });
+    
   };
 
   const handleClear = (e) => {
     e.preventDefault();
-    inputTextName.current.value = '';
-    inputTextLastname.current.value = '';
-    inputTextEmail.current.value = '';
-    inputTextDepartments.current.value = '';
-    inputTextTelInternal.current.value = '';
-    inputTextTelPrivate.current.value = '';
-    inputTextSocial.current.value = '';
+    inputTextBusinessName.current.value = '';
+    inputTextBusinessAddress.current.value = '';
+    inputTextRoad.current.value = '';
+    inputTextSubdistrict.current.value = '';
+    inputTextDistrict.current.value = '';
+    inputTextProvince.current.value = '';
+    inputTextZipcode.current.value = '';
+    inputTextTelephone.current.value = '';
   };
 
   return (
@@ -86,30 +113,46 @@ function AddUserForm(props) {
           fullWidth
           label={<Div>ชื่อร้านค้า</Div>}
           id="businessname"
-          inputRef={inputTextName}
+          inputRef={inputTextBusinessName}
           onChange={(e) => handleChangeBusinessName(e.target.value)}
         />
       </Box>
       <Box sx={{ mt: 1, display: 'flex' }}>
         <TextField
-          sx={{ maxWidth: '70%', padding: 1 }}
+          sx={{ maxWidth: '40%', padding: 1 }}
           label={<Div>ที่อยู่</Div>}
           id="businessAddress"
-          inputRef={inputTextLastname}
+          inputRef={inputTextBusinessAddress}
           onChange={(e) => handleChangeBusinessAddress(e.target.value)}
         />
+        <TextField
+          sx={{ maxWidth: '70%', padding: 1 }}
+          label={<Div>ซอย</Div>}
+          id="soi"
+          inputRef={inputTextSoi}
+          onChange={(e) => handleChangeSoi(e.target.value)}
+        />
+        <TextField
+          sx={{ maxWidth: '70%', padding: 1 }}
+          label={<Div>ถนน</Div>}
+          id="road"
+          inputRef={inputTextRoad}
+          onChange={(e) => handleChangeRoad(e.target.value)}
+        />
+      </Box>
+      <Box sx={{ mt: 1, display: 'flex' }}>
         <TextField
           sx={{ maxWidth: '55%', padding: 1 }}
           label={<Div>แขวง / ตำบล</Div>}
           id="subdistrict"
-          inputRef={inputTextDepartments}
+          inputRef={inputTextSubdistrict}
           onChange={(e) => handleChangeSubdistrict(e.target.value)}
         />
         <TextField
           sx={{ maxWidth: '55%', padding: 1 }}
           label={<Div>อำเภอ</Div>}
           id="district"
-          inputRef={inputTextTelInternal}
+          inputRef={inputTextDistrict}
           onChange={(e) => handleChangeDistrict(e.target.value)}
         />
       </Box>
@@ -119,17 +162,18 @@ function AddUserForm(props) {
           fullWidth
           label={<Div>จังหวัด</Div>}
           id="province"
-          inputRef={inputTextEmail}
+          inputRef={inputTextProvince}
           onChange={(e) => handleChangeProvince(e.target.value)}
         />
       </Box>
+
       <Box sx={{ mt: 1, display: 'flex' }}>
         <TextField
           sx={{ maxWidth: '100%', padding: 1 }}
           fullWidth
           label={<Div>รหัสไปรษณีย์</Div>}
           id="zipcode"
-          inputRef={inputTextTelPrivate}
+          inputRef={inputTextZipcode}
           onChange={(e) => handleChangeZipcode(e.target.value)}
         />
       </Box>
@@ -139,7 +183,7 @@ function AddUserForm(props) {
           fullWidth
           label={<Div>เบอร์โทร</Div>}
           id="telephone"
-          inputRef={inputTextSocial}
+          inputRef={inputTextTelephone}
           onChange={(e) => handleChangeTelephone(e.target.value)}
         />
       </Box>
